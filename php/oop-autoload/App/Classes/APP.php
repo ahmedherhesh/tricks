138e764ad2bef409dfdac478263afa4f8423ce2f
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-12-08 15:22:48 +0200

    first commit

---
+++ php/oop-autoload/App/Classes/APP.php
@@ -0,0 +1,8 @@
+<?php 
+namespace App\Classes;
+
+class App{
+    public static function hello(){
+        echo 'Hello Dr Abdelfatah';
+    }
+}
---
+++ php/oop-autoload/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'autoload.php';
+use App\Classes\App;
+
+App::hello();
