365a92d7c144b170c0e81caeae8c60f0fe8f8a0e
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-12-11 14:24:52 +0200

    after thinking

--- php/pages-handling/index.php
+++ php/pages-handling/index.php
@@ -4,7 +4,7 @@ function view(string $page, array $fields)
 {
     $finalPage = file_get_contents("views/$page.html");
     foreach ($fields as $key => $field) {
-        $finalPage = str_replace('{{$'.$key.'}}', $field, $finalPage);
+        $finalPage = str_replace('{{$' . $key . '}}', $field, $finalPage);
     }
     return $finalPage;
 }
@@ -13,8 +13,13 @@ function index()
 {
     $title = 'View Page';
     $heading = 'Welcome Back';
-    echo view(page: 'index', fields: ['title' => $title , 'heading' => $heading]);
+    echo view(page: 'index', fields: ['title' => $title, 'heading' => $heading]);
 }
 
-// you can call function like this way index() or 'index'() think about that;
+//you can call function like this way index() or 'index'() think about that;
 'index'();
+
+//after thinking we can do it by this way 
+$function_name = explode('/', $_SERVER['REQUEST_URI'])[1];
+if (function_exists($function_name))
+    $function_name();
