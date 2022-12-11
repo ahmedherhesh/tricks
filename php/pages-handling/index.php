8d467d58cbc0935d3e3b2a9686050111fad0b198
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-12-11 13:57:03 +0200

    first commit

---
+++ php/pages-handling/index.php
@@ -0,0 +1,19 @@
+<?php
+
+function view(string $page, array $fields)
+{
+    $finalPage = file_get_contents("views/$page.html");
+    foreach ($fields as $key => $field) {
+        $finalPage = str_replace('{{$'.$key.'}}', $field, $finalPage);
+    }
+    return $finalPage;
+}
+
+function index()
+{
+    $title = 'View Page';
+    $heading = 'Welcome Back';
+    echo view(page: 'index', fields: ['title' => $title , 'heading' => $heading]);
+}
+
+index();
